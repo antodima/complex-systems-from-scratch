7a781f058b8a48cec2d5572dd8ad70b0e0cfaee7
antodima <antonio.di.mauro.23@gmail.com>
2020-02-28 19:13:05 +0100

    added LotkaVolterraModel, SIRModel and PPVModel exercise

--- complex_systems/continuous_complex_systems.py
+++ complex_systems/continuous_complex_systems.py
@@ -9,6 +9,22 @@ class ContinuousComplexSystem(object):
         """Evolve the system.
         """
         raise NotImplementedError
+        
+    def deltas(self, y, t):
+        """Computes the derivatives of the differential equations.
+        
+        Parameters
+        ----------
+        y : list
+            the values of the system
+        t : numpy.linspace
+            the time points
+        
+        Returns
+        -------
+        the partial derivatives
+        """
+        raise NotImplementedError
 
     def plot(self):
         """Plot the evolution of the system.
@@ -28,20 +44,7 @@ class OregonatorModel(ContinuousComplexSystem):
         self.y = None # the solution/evolution of the system
         self.t = t # the time points
         
-    def __deltas(self, y, t):
-        """Computes the derivatives of the differential equations.
-        
-        Parameters
-        ----------
-        y : list
-            the values of the system
-        t : numpy.linspace
-            the time points
-        
-        Returns
-        -------
-        the partial derivatives
-        """
+    def deltas(self, y, t):
         A = y[0]
         B = y[1]
         C = y[2]
@@ -51,19 +54,133 @@ class OregonatorModel(ContinuousComplexSystem):
         return [dA, dB, dC]
     
     def evolve(self, y0):
-        """Calls the Ordinary Differential Equation solver (ODE).
-        
-        Parameters
-        ----------
-        y0 : list
-            the initial condition of the system
-        """
-        self.y = odeint(self.__deltas, y0, self.t)
+        self.y = odeint(self.deltas, y0, self.t)
     
     def plot(self):
+        plt.figure('OregonatorModel')
         plt.plot(self.t, self.y[:, 0], label='A')
         plt.plot(self.t, self.y[:, 1], label='B')
         plt.plot(self.t, self.y[:, 2], label='C')
+        plt.xlabel('Time')
+        plt.ylabel('Value')
+        plt.legend(loc='upper right')
+        plt.show()
+
+
+class LotkaVolterraModel(ContinuousComplexSystem):
+    """Lotka-Volterra model of predator-prey system.
+    """
+    
+    def __init__(self, t, growth_rate=10, death_rate=10, meeting_rate=0.01, n_offsprings=1):
+        self.r = growth_rate
+        self.s = death_rate
+        self.a = meeting_rate # predator-prey meetings
+        self.b = n_offsprings # number of offsprings produced for each hunting
+        self.V = None # preys
+        self.P = None # predators
+        self.t = t
+        self.y = None
+        
+    def deltas(self, y, t):
+        V = y[0]
+        P = y[1]
+        dV = self.r * V - self.a * V * P
+        dP = - self.s * P + self.a * self.b * V * P
+        return [dV, dP]
+        
+    def evolve(self, y0):
+        self.y = odeint(self.deltas, y0, self.t)
+        
+    def plot(self):
+        plt.figure('LotkaVolterraModel')
+        plt.plot(self.t, self.y[:, 0], label='V')
+        plt.plot(self.t, self.y[:, 1], label='P')
+        plt.xlabel('Time')
+        plt.ylabel('Value')
+        plt.legend(loc='upper right')
+        plt.show()
+
+
+class SIRModel(ContinuousComplexSystem):
+    """Susceptible-Infected-Recovered/Resistant model of epidemic phenomena.
+    S + I + R = 1
+    """
+    
+    def __init__(self, t, infection_coefficient=6, recovery_rate=2, growth_rate=2, vaccination_rate=0.1):
+        self.S = None
+        self.I = None
+        self.R = None
+        self.beta = infection_coefficient
+        self.gamma = recovery_rate
+        self.mi = growth_rate # μ
+        self.p = vaccination_rate
+        self.t = t
+        self.y = None
+        
+    def deltas(self, y, t):
+        S = y[0]
+        I = y[1]
+        R = y[2]
+        dS = (1 - self.p) * self.mi - self.beta * S * I - self.mi * S
+        dI = self.beta * S * I - self.gamma * I - self.mi * I
+        dR = self.p * self.mi + self.gamma * I - self.mi * R
+        return [dS, dI, dR]
+    
+    def evolve(self, y0):
+        self.y = odeint(self.deltas, y0, self.t)
+    
+    def vaccination_threshold(self):
+        return 1 - (self.mi + self.gamma) / self.beta
+    
+    def plot(self):
+        plt.figure('SIRModel')
+        plt.plot(self.t, self.y[:, 0], label='S')
+        plt.plot(self.t, self.y[:, 1], label='I')
+        plt.plot(self.t, self.y[:, 2], label='R')
+        plt.xlabel('Time')
+        plt.ylabel('Value')
+        plt.legend(loc='upper right')
+        plt.show()
+
+
+class PPVModel(ContinuousComplexSystem):
+    """Predator.Prey-Vegetation Model Exercise.
+    
+    Define a variant of the Lotka-Volterra model with 3 kinds of individual:
+        predator, prey and vegetation.
+    Predators eat preys, preys eat vegetation, and vegetation 
+    (in the absence of preys) grows exponentially.
+    """
+    
+    def __init__(self, t, growth_rate=10, death_rate=10, meeting_rate=0.01, n_offsprings=1, growth_rate_veg=2):
+        self.r = growth_rate
+        self.s = death_rate
+        self.a = meeting_rate # predator-prey meetings
+        self.b = n_offsprings # number of offsprings produced for each hunting
+        self.v = growth_rate_veg # vegetation growth rate
+        self.V = None # preys
+        self.P = None # predators
+        self.F = None # vegetations
+        self.t = t
+        self.y = None
+        
+    def deltas(self, y, t):
+        V = y[0]
+        P = y[1]
+        F = y[2]
+        dV = self.r * V - self.a * V * P
+        dP = - self.s * P + self.a * self.b * V * P
+        dF = (F ** self.v) - V
+        return [dV, dP, dF]
+        
+    def evolve(self, y0):
+        self.y = odeint(self.deltas, y0, self.t)
+        
+    def plot(self):
+        plt.figure('LotkaVolterraModel')
+        plt.plot(self.t, self.y[:, 0], label='V')
+        plt.plot(self.t, self.y[:, 1], label='P')
+        plt.plot(self.t, self.y[:, 2], label='F')
         plt.xlabel('Time')
         plt.ylabel('Value')
         plt.legend(loc='upper right')
--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from complex_systems import continuous_complex_systems as continuous
 
 linear_model = discrete.LinearModel(rd=1.5, sd=0.9, beta=10)
 linear_model.evolve(initial_population=20, t_max=20)
-print('LinearModel evolution at time', 5, linear_model.evolution_at_t(5))
-print('LinearModel equilibrium:', linear_model.equilibrium())
+#print('LinearModel evolution at time', 5, linear_model.evolution_at_t(5))
+#print('LinearModel equilibrium:', linear_model.equilibrium())
 #linear_model.plot()
 
 non_linear_model = discrete.NonLinearModel(rd=2, k=50)
@@ -25,4 +25,29 @@ y0 = [1, 1, 2]
 t = np.linspace(0, 2, 120000)
 oregonator_model = continuous.OregonatorModel(t)
 oregonator_model.evolve(y0)
-oregonator_model.plot()
+#oregonator_model.plot()
+
+y0 = [800, 1000]
+#y0 = [200, 200]
+t = np.linspace(0, 4, 2000)
+lv_model = continuous.LotkaVolterraModel(t)
+lv_model.evolve(y0)
+#lv_model.plot()
+
+y0 = [0.99, 0.01, 0]
+t = np.linspace(0, 10, 10)
+sir_model =  continuous.SIRModel(t, infection_coefficient=1800, 
+                                 vaccination_rate=0.5,
+                                 recovery_rate=100,
+                                 growth_rate=0.02)
+sir_model.evolve(y0)
+#sir_model.plot()
+#print('Vaccination threshold', sir_model.vaccination_threshold())
+
+y0 = [800, 1000, 5]
+#y0 = [200, 200]
+t = np.linspace(0, 4, 2000)
+ppv_model = continuous.PPVModel(t)
+ppv_model.evolve(y0)
+#ppv_model.plot()
+
